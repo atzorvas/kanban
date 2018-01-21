ab1ed37ea593eb1beaa436b4bb0065b1b7e13246
Antonis Tzorvas <antonis@tzorvas.com>
2018-01-21 15:28:04 +0200

    6.5/6.6 Editing notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -26,7 +26,11 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} onDelete={this.deleteNote} />
+        <Notes
+          notes={notes}
+          onNoteClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote} />
       </div>
     );
   }
@@ -45,6 +49,31 @@ export default class App extends React.Component {
 
     this.setState({
       notes: this.state.notes.filter(note => note.id !== id)
+    });
+  }
+
+  activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
+
+        return note;
+      })
+    });
+  }
+
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+
+        return note;
+      })
     });
   }
 }
--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -5,11 +5,33 @@ export default({editing, value, onEdit, ...props}) => {
     return <Edit value={value} onEdit={onEdit} {...props} />;
   }
 
-  return <span {...props}>value: {value}</span>;
-}
+  return <span {...props}>{value}</span>;
+};
 
-const Edit = ({onEdit = () => {}, value, ...props}) => (
-  <div onClick={onEdit} {...props}>
-    <span>edit: {value}</span>
-  </div>
-);
+class Edit extends React.Component {
+  render() {
+    const {value, onEdit, ...props} = this.props;
+
+    return  <input
+      type="text"
+      autoFocus={true}
+      defaultValue={value}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter}
+      {...props} />;
+  }
+
+  checkEnter = (e) => {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+
+  finishEdit = (e) => {
+    const value = e.target.value;
+
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+    }
+  }
+}
